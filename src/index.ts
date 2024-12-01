26b2a2d96dddd01702a09dac7e0b2024c2dc2408
Gene Aumson <feuGeneA@users.noreply.github.com>
2024-11-30 20:05:41 -0500

    generate key and print address
    60 minutes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,75 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+// TODO: encrypt private key before writing to (and decrypt upon read from)
+// disk.
 
-program.name("sol-wallet").option("--first");
+// TODO: allow user to specify a key file to use instead of the default of
+// $HOME/sol-wallet.key
+
+import fs from "node:fs/promises";
+import os from "node:os";
+import process from "node:process";
+process.removeAllListeners("warning");
+
+import { program } from "commander";
+import { Keypair } from "@solana/web3.js";
+
+const keyfile = os.homedir() + "/sol-wallet.key";
+
+program.name("sol-wallet");
+
+program
+  .command("generate")
+  .description("generate a new keypair")
+  .action(generate);
+
+program
+  .command("address")
+  .description("print the public wallet address")
+  .action(address);
+
+async function generate() {
+  const keypair = Keypair.generate();
+  try {
+    await fs.writeFile(keyfile, keypair.secretKey, {
+      flag: "wx",
+    });
+  } catch (err) {
+    console.error("Failed to write generated key to file at", keyfile);
+    throw err;
+  }
+}
+
+async function address() {
+  const secretKey = await (async () => {
+    try {
+      return await fs.readFile(keyfile);
+    } catch (err) {
+      if (
+        typeof err === "object" &&
+        err &&
+        "code" in err &&
+        err.code === "ENOENT"
+      ) {
+        console.error(
+          "Key file does not exist. Consider using the generate command to create one.",
+        );
+      }
+      throw err;
+    }
+  })();
+  const keypair = (() => {
+    try {
+      return Keypair.fromSecretKey(secretKey);
+    } catch (err) {
+      console.error(
+        "Failed to create keypair from the contents of the file at",
+        keyfile,
+      );
+      throw err;
+    }
+  })();
+  console.log(keypair.publicKey.toString());
+}
 
 program.parse();
-
-const options = program.opts();
-console.log("args[0]:", program.args[0]);
-console.log("options:", options);
